0c1ce1c066e46389e6e4e5f7653b5eb0e64d270a
shifna <shifna@luminartechnolab.com>
2022-09-24 09:48:51 +0530

    login created

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  aim='your perfect banking partner'
+  acnt='enter your ac number'
+
+  acno=''
+  psw=''
+
+  userDetails:any={
+    1000:{acno:1000,username:"amal",password:123,balance:100000},
+    1001:{acno:1002,username:"anu",password:123,balance:200000},
+    1002:{acno:1002,username:"joyal",password:123,balance:150000},
+    1003:{acno:1003,username:"anaga",password:123,balance:100000},
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
+  login(){
+    var acnum=this.acno
+    var psw=this.psw
+    let userDetails=this.userDetails
+    if(acnum in userDetails){
+      if(psw==userDetails[acnum]['password']){
+        alert('login success')
+
+      }
+      else{
+        alert('incurrect password')
+      }
+
+    }
+    else{
+      alert('user not exist or incurrect ac number')
+    }
+    
+  }
+  acnoChange(event:any){
+    this.acno=event.target.value
+    console.log(this.acno);    
+  }
+  pswChange(event:any){
+    this.psw=event.target.value
+    console.log(this.psw);
+    
+  }
+
+
+}
+
+
